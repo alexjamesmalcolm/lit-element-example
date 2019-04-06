9e14815cfaea7bd067cf5e43124e331c8575177b
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-04-06 15:52:12 -0400

    Rendering properties

--- src/app.js
+++ src/app.js
@@ -2,13 +2,34 @@ import { LitElement, html } from "lit-element";
 
 class MyApp extends LitElement {
 
+    static get properties() {
+        return {
+            username: { type: String },
+            messages: { type: Array }
+        }
+    }
+
     constructor() {
         super();
+        this.username = "Alex Malcolm";
+        this.messages = [
+            "(Urgent) Please respond",
+            "Doctors hate him, see why"
+        ]
     }
 
     render() {
         return html`
-            <p>Hello World!</p>
+            <header>
+                <p>Hello ${this.username}</p>
+            </header>
+            <main>
+                <p>Here are your messages</p>
+                <ul>
+                    ${this.messages.map(message => html`<li>${message}</li>`)}
+                </ul>
+            </main>
+            <footer></footer>
         `;
     }
 }
